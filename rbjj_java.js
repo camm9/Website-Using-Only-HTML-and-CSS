db878fa1e2742b6e7b41217c66bc4e566d7507f7
camm9 <105569979+camm9@users.noreply.github.com>
2022-06-13 20:42:27 -0700

    First Update

---
+++ rbjj_java.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+CSIS 1280 Multimedia Web Development
+
+Project: Resenha BJJ Website
+Author: Catherine Methven
+Date:   June 8 2022
+
+Filename: rbjj_java.js
+
+*/
+
+/*Affiliate Image on Aside 
+
+<script type="text/javascript" src="//cdn.refersion.com/creative.js"></script>
+<script>$rfsn_creative.generate('refersion_client/31261/creatives/dynamic/39781-b3c82659d858408702ef7170cf130a8a.json', {
+	aid: '6564888.517789'
+});</script><div id="rfsn_img_39781"></div>
+
+*/
