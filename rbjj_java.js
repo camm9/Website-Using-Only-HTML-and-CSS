336e587ab8e8533fa1ef21b3a97dee292f495485
camm9 <105569979+camm9@users.noreply.github.com>
2022-07-06 17:40:35 -0700

    JS idea

--- rbjj_java.js
+++ rbjj_java.js
@@ -9,13 +9,8 @@ Date:   June 8 2022
 
 Filename: rbjj_java.js
 
-*/
 
-/*Affiliate Image on Aside 
 
-<script type="text/javascript" src="//cdn.refersion.com/creative.js"></script>
-<script>$rfsn_creative.generate('refersion_client/31261/creatives/dynamic/39781-b3c82659d858408702ef7170cf130a8a.json', {
-	aid: '6564888.517789'
-});</script><div id="rfsn_img_39781"></div>
+Create a size chart for product that pops up in a window??
 
 */
